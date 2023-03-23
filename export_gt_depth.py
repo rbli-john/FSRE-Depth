56bdb639a1f2b73282124aa22e8115e6695f34d6
grcgs2212 <ying1029@stanford.edu>
2023-03-22 19:00:51 -0700

    gt

--- export_gt_depth.py
+++ export_gt_depth.py
@@ -18,16 +18,19 @@ def export_gt_depths_kitti():
                         type=str,
                         help='which split to export gt from',
                         default='eigen',
-                        choices=["eigen", "eigen_benchmark"])
+                        choices=["eigen", "eigen_benchmark", "predictions"]) # "predictions" for small batch of self selected images
     opt = parser.parse_args()
 
-    split_folder = os.path.join(os.path.dirname(__file__), "splits", opt.split)
+    if args.split == "predictions":
+        split_folder = os.path.join(os.path.dirname(__file__), "predictions", "image")
+    else:
+        split_folder = os.path.join(os.path.dirname(__file__), "splits", opt.split)
     lines = readlines(os.path.join(split_folder, "test_files.txt"))
 
     print("Exporting ground truth depths for {}".format(opt.split))
 
     gt_depths = []
-    for line in lines:
+    for idx, line in enumerate(lines):
 
         folder, frame_id, _ = line.split()
         frame_id = int(frame_id)
@@ -37,10 +40,27 @@ def export_gt_depths_kitti():
             velo_filename = os.path.join(opt.data_path, folder,
                                          "velodyne_points/data", "{:010d}.bin".format(frame_id))
             gt_depth = generate_depth_map(calib_dir, velo_filename, 2, True)
+
         elif opt.split == "eigen_benchmark":
             gt_depth_path = os.path.join(opt.data_path, folder, "proj_depth",
                                          "groundtruth", "image_02", "{:010d}.png".format(frame_id))
             gt_depth = np.array(pil.open(gt_depth_path)).astype(np.float32) / 256
+
+        elif opt.split == "predictions":
+            calib_dir = os.path.join(opt.data_path, folder.split("/")[0])
+            velo_filename = os.path.join(opt.data_path, folder,
+                                         "velodyne_points/data", "{:010d}.bin".format(frame_id))
+            gt_depth = generate_depth_map(calib_dir, velo_filename, 2, True)
+            print("gt_depth:", gt_depth.shape)
+
+            # heat map
+            save_path = os.path.join("predictions", "ground_truth", "{:010d}.png".format(idx))
+            plt.clf()
+            plt.figure(figsize = (17, 5), dpi=100)
+            plt.imshow(depth, cmap=plt.get_cmap('plasma'), interpolation='nearest')
+            plt.axis('off')
+            plt.savefig(save_path, bbox_inches='tight', pad_inches=0)
+
 
         gt_depths.append(gt_depth.astype(np.float32))
 
